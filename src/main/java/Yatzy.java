7b8f9104deb22f9c6260e762253f8976fe2343c1
hjmii <hjmii@ITEM-S106050.mshome.net>
2023-03-26 03:41:19 +0200

    refactor ones,twos,threes, fours,fives, sixes

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -16,79 +16,32 @@ public class Yatzy {
 	dice[4] = d5;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-	int sum = 0;
-	if (d1 == 1)
-	    sum++;
-	if (d2 == 1)
-	    sum++;
-	if (d3 == 1)
-	    sum++;
-	if (d4 == 1)
-	    sum++;
-	if (d5 == 1)
-	    sum++;
-
-	return sum;
+    public int ones() {
+	return sumOf(1);
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-	int sum = 0;
-	if (d1 == 2)
-	    sum += 2;
-	if (d2 == 2)
-	    sum += 2;
-	if (d3 == 2)
-	    sum += 2;
-	if (d4 == 2)
-	    sum += 2;
-	if (d5 == 2)
-	    sum += 2;
-	return sum;
+    public int twos() {
+	return sumOf(2);
     }
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-	int s;
-	s = 0;
-	if (d1 == 3)
-	    s += 3;
-	if (d2 == 3)
-	    s += 3;
-	if (d3 == 3)
-	    s += 3;
-	if (d4 == 3)
-	    s += 3;
-	if (d5 == 3)
-	    s += 3;
-	return s;
+    public int threes() {
+	return sumOf(3);
     }
 
     public int fours() {
-	int sum;
-	sum = 0;
-	for (int at = 0; at != 5; at++) {
-	    if (dice[at] == 4) {
-		sum += 4;
-	    }
-	}
-	return sum;
+	return sumOf(4);
     }
 
     public int fives() {
-	int s = 0;
-	int i;
-	for (i = 0; i < dice.length; i++)
-	    if (dice[i] == 5)
-		s = s + 5;
-	return s;
+	return sumOf(5);
     }
 
     public int sixes() {
-	int sum = 0;
-	for (int at = 0; at < dice.length; at++)
-	    if (dice[at] == 6)
-		sum = sum + 6;
-	return sum;
+	return sumOf(6);
+    }
+
+    private int sumOf(int die) {
+	return IntStream.of(dice).filter(d -> d == die).sum();
     }
 
     public static int chance(int d1, int d2, int d3, int d4, int d5) {
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -15,44 +15,63 @@ public class YatzyTest {
     }
 
     @Test
-    public void test_1s() {
-	assertTrue(Yatzy.ones(1, 2, 3, 4, 5) == 1);
-	assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
-	assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
-	assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
+    public void testOnes() {
+	int expected = 0;
+	int actual = new Yatzy(6, 2, 2, 4, 5).ones();
+	assertEquals(expected, actual);
+	assertEquals(1, new Yatzy(1, 2, 3, 4, 5).ones());
+	assertEquals(2, new Yatzy(1, 2, 1, 4, 5).ones());
+	assertEquals(5, new Yatzy(1, 1, 1, 1, 1).ones());
     }
 
     @Test
-    public void test_2s() {
-	assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6));
-	assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2));
+    public void testTwos() {
+	int expected = 0;
+	int actual = new Yatzy(1, 3, 4, 5, 6).twos();
+	assertEquals(expected, actual);
+	assertEquals(4, new Yatzy(1, 2, 3, 2, 6).twos());
+	assertEquals(10, new Yatzy(2, 2, 2, 2, 2).twos());
+	assertEquals(2, new Yatzy(6, 2, 4, 1, 6).twos());
     }
 
     @Test
-    public void test_threes() {
-	assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3));
-	assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3));
+    public void testThrees() {
+	int expected = 0;
+	int actual = new Yatzy(2, 1, 4, 5, 6).threes();
+	assertEquals(expected, actual);
+	assertEquals(3, new Yatzy(1, 2, 5, 2, 3).threes());
+	assertEquals(6, new Yatzy(1, 2, 3, 2, 3).threes());
+	assertEquals(15, new Yatzy(3, 3, 3, 3, 3).threes());
     }
 
     @Test
-    public void fours_test() {
-	assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
-	assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
+    public void testFours() {
+	int expected = 0;
+	int actual = new Yatzy(3, 5, 5, 5, 5).fours();
+	assertEquals(expected, actual);
 	assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
+	assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
+	assertEquals(20, new Yatzy(4, 4, 4, 4, 4).fours());
     }
 
     @Test
-    public void fives() {
+    public void testFives() {
+	int expected = 0;
+	int actual = new Yatzy(4, 3, 2, 1, 6).fives();
+	assertEquals(expected, actual);
+	assertEquals(5, new Yatzy(4, 4, 4, 3, 5).fives());
 	assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
-	assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
-	assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
+	assertEquals(25, new Yatzy(5, 5, 5, 5, 5).fives());
     }
 
     @Test
-    public void sixes_test() {
-	assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
+    public void testSixes() {
+	int expected = 0;
+	int actual = new Yatzy(4, 4, 4, 5, 5).sixes();
+	assertEquals(expected, actual);
 	assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
-	assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
+	assertEquals(12, new Yatzy(1, 5, 6, 6, 5).sixes());
+	assertEquals(30, new Yatzy(6, 6, 6, 6, 6).sixes());
     }
 
     @Test
